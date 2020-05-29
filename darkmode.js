6872a9215e95ecbc2b93a7d1b83e739ad2a4a21c
firesucka <32978063+firesucka@users.noreply.github.com>
2020-05-29 10:55:25 +0200

    dsa

--- darkmode.js
+++ darkmode.js
@@ -5,8 +5,8 @@ var options = {
   time: '0.3s', // default: '0.3s'
   mixColor: '#fff', // default: '#fff'
   backgroundColor: 'rgb(50, 50, 50)', // default: '#fff'
-  buttonColorDark: '#100f2c', // default: '#100f2c'
-  buttonColorLight: '#fff', // default: '#fff'
+  buttonColorDark: '#fff', // default: '#100f2c'
+  buttonColorLight: '#100f2c', // default: '#fff'
   saveInCookies: true, // default: true,
   label: '🌓', // default: ''
   autoMatchOsTheme: true // default: true
